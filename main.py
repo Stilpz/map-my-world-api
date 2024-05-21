86effa8992cd156ca383058a324f016206a37c24
Stiven Lopez <stivenlopezcar21@gmail.com>
2024-05-21 01:21:05 -0500

    Segundo commit de la API de prueba, ya con la inserción de las locaciones. Se construye primer modelo de datos.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-#http://127.0.0.1:8000
+class Location(BaseModel):
+    id: int
+    longitude: float
+    latitude: float
+    name: str
+    category: str
+    calification: Optional[str]
 
 @app.get("/")
 def index():
     return {"message" : "Bienvenidos al API Map My World'"}
+
+@app.get("/locations/{id}")
+def show_location(id: int):
+    return {"data" : id}
+
+@app.post("/locations")
+def insert_location(location: Location):
+    return {"message" : f"Location {location:name} inserted"}
